7dd47e539b1534426d333af8b209437179d2cb11
SidiJr <sednei.jr@gmail.com>
2025-07-03 19:49:34 -0300

    style: :art: Removendo arquivos antigos
    Removendo arquivos da versão anterior.

--- old-src/pages/Categorias/CategoriasList.jsx
+++
@@ -1,9 +0,0 @@
-import List from "../../components/List/List";
-
-const ListLocacoes = () => {
-  const fields = ["nome"];
-
-  return <List type="categorias" fields={fields} />;
-};
-
-export default ListLocacoes;
